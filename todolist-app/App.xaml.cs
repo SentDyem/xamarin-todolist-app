338fdcc0c1f49570058dcf59b9b4a5b13245db39
SentDyem <55694826+SentDyem@users.noreply.github.com>
2022-07-11 12:02:04 +0300

    writing ui

--- todolist-app/App.xaml.cs
+++ todolist-app/App.xaml.cs
@@ -10,7 +10,7 @@ namespace todolist_app
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage(new MainPage());
+            MainPage = new MainPage();
         }
 
         protected override void OnStart()
