d04c6227fb0c6266a3ccdfac88ab4f139f22e3f2
SentDyem <55694826+SentDyem@users.noreply.github.com>
2022-07-11 11:25:18 +0300

    import xamarin community toolkit, add toolbar on the main page

--- todolist-app/App.xaml.cs
+++ todolist-app/App.xaml.cs
@@ -10,7 +10,7 @@ namespace todolist_app
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
