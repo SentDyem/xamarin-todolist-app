fe11c2088fba3f06569a4b821571de99901899f8
SentDyem <55694826+SentDyem@users.noreply.github.com>
2022-07-14 23:48:23 +0300

    writing ui, import helper class for quckly access to icons

--- todolist-app/AssemblyInfo.cs
+++ todolist-app/AssemblyInfo.cs
@@ -1,3 +1,7 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("FontAwesome6Brands-Regular-400.otf", Alias = "FAB")]
+[assembly: ExportFont("FontAwesome6Free-Regular-400.otf", Alias = "FAR")]
+[assembly: ExportFont("FontAwesome6Free-Solid-900.otf", Alias = "FAS")]
