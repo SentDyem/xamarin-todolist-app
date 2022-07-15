58c6cb51581de90acac65ff79eaa99d96e2cdec9
SentDyem <55694826+SentDyem@users.noreply.github.com>
2022-07-15 14:35:01 +0300

    import sql database, small changes

---
+++ todolist-app/Models/Task.cs
@@ -0,0 +1,16 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace todolist_app.Models
+{
+    class Task
+{
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        public string Note { get; set; }
+
+        public DateTime Time { get; set; }
+}
---
+++ todolist-app/Services/TaskService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace todolist_app.Services
+{
+    class TaskService
+{
+}
+}
